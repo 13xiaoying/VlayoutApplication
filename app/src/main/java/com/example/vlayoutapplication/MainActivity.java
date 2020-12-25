7372c619168f8a164831a7c3dfb74804cec5b889
13xiaoying <2765006582@qq.com>
2020-12-25 11:55:26 +0800

    第一次提交

---
+++ app/src/main/java/com/example/vlayoutapplication/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.vlayoutapplication;
+
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * 首页
+     */
+    private RadioButton mButtonOne;
+    /**
+     * 专题
+     */
+    private RadioButton mButtonTwo;
+    /**
+     * 分类
+     */
+    private RadioButton mButtonThree;
+    /**
+     * 购物车
+     */
+    private RadioButton mButtonFour;
+    /**
+     * 我的
+     */
+    private RadioButton mButtonFive;
+    private RadioGroup mRadio;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mButtonOne = (RadioButton) findViewById(R.id.button_one);
+        mButtonTwo = (RadioButton) findViewById(R.id.button_two);
+        mButtonThree = (RadioButton) findViewById(R.id.button_three);
+        mButtonFour = (RadioButton) findViewById(R.id.button_four);
+        mButtonFive = (RadioButton) findViewById(R.id.button_five);
+        mRadio = (RadioGroup) findViewById(R.id.radio);
+    }
+}
